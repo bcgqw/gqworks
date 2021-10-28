ccf45edd67a8eed1831bb891a65923206e029c80
bcgqw <bryancarranza1@outlook.com>
2021-10-28 22:43:27 +0000

    Switched to Twenty Twenty theme

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -52,6 +52,24 @@ class ComposerAutoloaderInit8dbf51442a84a66e14e4bf44f8407f37
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit8dbf51442a84a66e14e4bf44f8407f37::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire8dbf51442a84a66e14e4bf44f8407f37($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire8dbf51442a84a66e14e4bf44f8407f37($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
